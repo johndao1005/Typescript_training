c969019354f90b5f12e94d557a56c05c4f3388a6
John Dao | BlueBerryIT <john.dao@blueberryit.co.nz>
2023-04-19 10:35:57 +1200

    testing

--- first_week.ts
+++ first_week.ts
@@ -1,47 +1,54 @@
 export class Clock {
   constructor(hour: unknown, minute?: unknown) {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 
   public toString(): unknown {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 
   public plus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 
   public minus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 
   public equals(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 }
 
 const settings = {
-  "API_Table_pair":[
+  API_Table_pair: [
     {
-      "API":"TaskSecondaryResources",
-      "TableName":"cr96b_stagingbbbitasksecondaryresources"
-    },{
-      "API":"TaskNotess",
-      "TableName":"cr96b_stagingbbbitasknotes"
+      API: "TaskSecondaryResources",
+      TableName: "cr96b_stagingbbbitasksecondaryresources",
     },
-    {"API":"table_pair"},
-    {"API":"table_pair"},
-    {"API":"table_pair"},
-    {"API":"table_pair"},
-    {"API":"table_pair"},
-    {"API":"table_pair"},
-    {"API":"table_pair"},
-    {"API":"table_pair"},
-    {"API":"table_pair"},
-  ]
-} // make API name and staging table name pair
-
+    {
+      API: "TaskNotes",
+      TableName: "cr96b_stagingbbbitasknotes",
+    },
+    { API: "table_pair" },
+    { API: "table_pair" },
+    { API: "table_pair" },
+    { API: "table_pair" },
+    { API: "table_pair" },
+    { API: "table_pair" },
+    { API: "table_pair" },
+    { API: "table_pair" },
+    { API: "table_pair" },
+  ],
+}; // make API name and staging table name pair
+// outputs('API_and_Table_Names_-_Projects')['TableName']
 // outputs('Settings')['API_Table_pair'] - get the output from a list
 //items('Apply_to_each')?[0]
+
+// body('Parse_JSON')?['items']
+
 // cr96b_stagingbbbiprojects
-// body('Parse_JSON')?['TableName']
+// outputs('API_and_Table_Name')?['TableName'],
+// body('Get_API_and_Table_Name')?['API'] ,body('Get_query_count')?['queryCount'], length(outputs('Get_Current_Records')?['body/value'])
+// outputs('List_rows_2')?['body/value'][add(variables('Rows_Count'),-1)]
+//  outputs('Get_Current_Records')?['body/value'][add(variables('Rows_Count'),-1)]['cr96b_itemsid'] 
